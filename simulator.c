edcd0664f187c164dd6b51c85c3a8578c68b93d2
Anes Belfodil <ans.belfodil@gmail.com>
2019-02-22 12:07:50 -0500

    This bunny should be killed.

--- simulator.c
+++ simulator.c
@@ -3,16 +3,9 @@
 
 const struct avr_mmcu_vcd_trace_t traces[]  _MMCU_ = {
 		{AVR_MCU_VCD_SYMBOL("PORTA"), .what = (void*) &PORTA,},
-		{AVR_MCU_VCD_SYMBOL("PINA"), .what = (void*) &PINA,},
-
 		{AVR_MCU_VCD_SYMBOL("PORTB"), .what = (void*) &PORTB,},
-		{AVR_MCU_VCD_SYMBOL("PINB"), .what = (void*) &PINB,},
-
 		{AVR_MCU_VCD_SYMBOL("PORTC"), .what = (void*) &PORTC,},
-		{AVR_MCU_VCD_SYMBOL("PINC"), .what = (void*) &PINC,},
-
 		{AVR_MCU_VCD_SYMBOL("PORTD"), .what = (void*) &PORTD,},
-		{AVR_MCU_VCD_SYMBOL("PIND"), .what = (void*) &PIND,},
 };
 
 #define VOLTAGE 5000U
