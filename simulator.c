a5f45faa0eaf71f21db5b94ff2ae8ac511b6c4cc
Anes Belfodil <ans.belfodil@gmail.com>
2020-05-03 18:41:04 -0400

    Herpderp, shoulda check if it does really compile.

--- simulator.c
+++ simulator.c
@@ -1,12 +1,15 @@
 #include <avr/io.h>
 #include <simavr/avr/avr_mcu_section.h>
 
+#define F_CPU 8000000UL
+AVR_MCU(F_CPU, "atmega324pa");
+
+#define VOLTAGE 5000U
+AVR_MCU_VOLTAGES(VOLTAGE, VOLTAGE, VOLTAGE)
+
 const struct avr_mmcu_vcd_trace_t traces[]  _MMCU_ = {
 		{AVR_MCU_VCD_SYMBOL("PORTA"), .what = (void*) &PORTA,},
 		{AVR_MCU_VCD_SYMBOL("PORTB"), .what = (void*) &PORTB,},
 		{AVR_MCU_VCD_SYMBOL("PORTC"), .what = (void*) &PORTC,},
 		{AVR_MCU_VCD_SYMBOL("PORTD"), .what = (void*) &PORTD,},
 };
-
-#define VOLTAGE 5000U
-AVR_MCU_VOLTAGES(VOLTAGE, VOLTAGE, VOLTAGE)
--- test_program.cpp
+++ test_program.cpp
@@ -1,16 +1,20 @@
 #include <avr/io.h>
+#include "can.h"
 
 int main()
 {
-	DDRA = 0xff;
+	DDRA = 0x00;
 	DDRB = 0xff;
 	DDRC = 0xff;
 	DDRD = 0x00;
+
 	unsigned long counter = 0;
+	can can{};
 
 	while (true) {
+		// uint8_t volt = can.lecture(0x0) >> 2;
 		if (PIND & 0b100) {
-			PORTA = ++counter;
+			PORTB = ++counter;
 		}
 	}
 }
